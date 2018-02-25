715998c66b71f75861bf3a8f644fe1ced6ebd543
GuilinZhu <memory140662@gmail.com>
2018-02-25 23:33:40 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/Todo');
 const { User } = require('./models/User');
@@ -23,6 +24,20 @@ app.get('/todos', (req, res) => {
     Todo.find()
         .then(todos => res.send({todos}))
         .catch(e => res.status(400).send(e));
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+    let { id } = req.params;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then(todo => {
+        if (!todo) return res.status(404).send();
+
+        res.send({todo});
+    }).catch(e => res.status(400).send(e));
 });
 
 app.listen(3000, () => console.log('Started on port 3000'));
